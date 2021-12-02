1d3580c6c7adeb9bd3a28c8fe30d7dbc12b57358
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-03 00:56:30 +0800

    init

---
+++ lib/ecto_timescaledb.ex
@@ -0,0 +1,18 @@
+defmodule EctoTimescaledb do
+  @moduledoc """
+  Documentation for `EctoTimescaledb`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoTimescaledb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_timescaledb_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoTimescaledbTest do
+  use ExUnit.Case
+  doctest EctoTimescaledb
+
+  test "greets the world" do
+    assert EctoTimescaledb.hello() == :world
+  end
+end
