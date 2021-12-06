97b25e0c10df2e2519a725ff8f4143c7140abd79
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-06 18:06:46 +0800

    migration function

--- lib/migration.ex
+++ lib/migration.ex
@@ -3,21 +3,28 @@ defmodule Ecto.Migration.Timescaledb do
   Provides migration related functions in [TimescaleDB](https://www.timescale.com/) extended SQL for `Ecto.Query`
   """
 
-  @doc """
-  [create_hypertable](https://docs.timescale.com/api/latest/hypertable/create_hypertable/#create-hypertable)
-
-  ## Examples
-
-  ```
-  use Ecto.Migration
-  use Ecto.Migration.Timescaledb
-
-  select(create_hypertable(:stat, :time))
-  ```
-  """
-  defmacro create_hypertable(relation, time_column_name) do
+  def __using__(_options) do
     quote do
-      fragment(unquote("create_hypertable(?, ?)"), unquote(relation), unquote(time_column_name))
+      use Ecto.Migration
+
+      @doc """
+      [create_hypertable](https://docs.timescale.com/api/latest/hypertable/create_hypertable/#create-hypertable)
+
+      ## Examples
+
+      ```
+      use Ecto.Migration.Timescaledb
+
+      create_hypertable(:stat, :time)
+      ```
+      """
+      def create_hypertable(relation, time_column_name) do
+        execute(fn ->
+          repo().query!("SELECT create_hypertable($1, $2)", [relation, time_column_name],
+            log: :info
+          )
+        end)
+      end
     end
   end
 end
